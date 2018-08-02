1249948704b0ae1847ebe614801f8a326050b0f4
DavidMChan <david@iridescent.io>
2018-08-01 19:32:04 -0700

    Updated setup.py file

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,19 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='tsnecuda',
     version='0.1.0',
     author='Chan, David M., Huang, Forrest., Rao, Roshan.',
-    author_email='davidchan@berkeley.edu,forrest_huang@berkeley.edu,roshan_rao@berkeley.edu',
+    author_email='davidchan@berkeley.edu',
     packages=['tsnecuda', 'tsnecuda.test'],
     package_data={'tsnecuda': ['libtsnecuda.so']},
     scripts=[],
-    url='',
+    url='https://github.com/CannyLab/tsne-cuda',
     license='LICENSE.txt',
     description='CUDA Implementation of T-SNE with Python bindings',
     long_description=open('README.txt').read(),
     install_requires=[
-        "numpy >= 1.14.1",
+        'numpy >= 1.14.1',    
     ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    keywords=[
+        'TSNE',
+        'CUDA',
+        'Machine Learning',
+        'AI'
+    ]
+
 )
 
