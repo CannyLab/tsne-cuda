4dc4b739a7756fe3a1ad87afa45bdd8c16b79ca3
Roshan Rao <roshan_rao@berkeley.edu>
2018-08-30 17:16:04 -0700

    got it to compile under conda, but there's some weird error

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 git submodule init
 git submodule update 
 cd ./build
-cmake  .. -DBUILD_PYTHON=TRUE
+cmake  .. -DBUILD_PYTHON=TRUE -DWITH_MKL=FALSE -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
 make -j4
-make -j4
-make
-make
 cd python/
 pip install -e .
