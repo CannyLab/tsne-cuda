aa50f2ccb25918068b3b278814278dd843ce220f
DavidMChan <david@iridescent.io>
2018-09-06 15:02:10 -0700

    Working conda build. Suffering from #17

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ git submodule init
 git submodule update 
 cd ./build
 cmake  .. -DBUILD_PYTHON=TRUE -DWITH_MKL=FALSE -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
-make -j4
+make 
 cd python/
-pip install -e .
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+cp -r ../lib/* $PREFIX/lib/
