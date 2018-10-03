5041a73b4c362e901af289f6728bf8f14f229cf9
Roshan Rao <roshan_rao@berkeley.edu>
2018-10-03 16:10:11 -0700

    Need to run make twice for some reason

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+source ~/.bashrc
 git submodule init
 git submodule update 
 cd ./build
 cmake  .. -DBUILD_PYTHON=TRUE -DWITH_MKL=FALSE -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
-make 
+pwd
+make -j5 
+make
 cd python/
+pwd
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 cp -r ../lib/* $PREFIX/lib/
