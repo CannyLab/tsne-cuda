6a85dad7aab1c47beb9c0d91b377dddbfce10395
DavidMChan <david@iridescent.io>
2022-02-07 15:04:09 -0800

    Adjusted packaging for 3.0.1

--- packaging/run_all_packaging.sh
+++ packaging/run_all_packaging.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 # Build all of the docker files
+docker build . -f ./packaging/Dockerfile.cuda10.0 -t tsnecuda-docker-10.0
 docker build . -f ./packaging/Dockerfile.cuda10.1 -t tsnecuda-docker-10.1
 docker build . -f ./packaging/Dockerfile.cuda10.2 -t tsnecuda-docker-10.2
 docker build . -f ./packaging/Dockerfile.cuda11.0 -t tsnecuda-docker-11.0
@@ -12,6 +13,7 @@ docker build . -f ./packaging/Dockerfile.cuda11.2 -t tsnecuda-docker-11.2
 docker build . -f ./packaging/Dockerfile.cuda11.3 -t tsnecuda-docker-11.3
 
 # Run all of the docker files to get outputs
+docker run -it --mount src=$(realpath ./build),target='/artifacts',type=bind tsnecuda-docker-10.0:latest
 docker run -it --mount src=$(realpath ./build),target='/artifacts',type=bind tsnecuda-docker-10.1:latest
 docker run -it --mount src=$(realpath ./build),target='/artifacts',type=bind tsnecuda-docker-10.2:latest
 docker run -it --mount src=$(realpath ./build),target='/artifacts',type=bind tsnecuda-docker-11.0:latest
@@ -24,6 +26,13 @@ sudo chown -R $(whoami):$(whoami) ./build
 
 # Run the anaconda packaging and upload
 conda-build -c pytorch ./packaging/conda/
+
+# Run the local pip packaging and upload
+cd ./build/build_10.1
+cat VERSION.txt | awk 'NF{print $0 "+cu100"}' > tmp && mv tmp VERSION.txt
+python3 setup.py bdist_wheel
+mv dist/*.whl ../../packaging/pypi_extra_hosts/dist/
+cd ../../
 
 # Run the local pip packaging and upload
 cd ./build/build_10.1
